54347e316f11187f5ebff832b38136009889b7e7
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-09-10 17:32:52 +0100

    committing basic arithmetric exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum_of_numbers = number1 + number2
+difference_of_numbers = number1 - number2
+product_of_numbers = number1 * number2
+print("Addition of",number1,"and",number2,"is",sum_of_numbers)
+print("Subtraction of",number1,"and",number2,"is",difference_of_numbers)
+print("Multiplication of",number1,"and",number2,"is",product_of_numbers)
+
