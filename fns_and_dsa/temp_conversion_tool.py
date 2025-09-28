84d84b2245bab8498019cf0e11b5a0101f6b6d51
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-09-28 13:36:26 +0100

    third commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,11 +2,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * fahrenheit
+    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit-32)
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR * celsius
+    fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius + 32)
     return fahrenheit
 
 def main():
