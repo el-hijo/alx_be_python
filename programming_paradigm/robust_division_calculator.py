5a23006cc3f6c00bf5681fb456701b1d717b06b4
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-10-05 15:33:52 +0100

    realy made numerator and denominator float

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,7 @@
-def safe_divide(float(numerator), float(denominator)):
+def safe_divide(numerator, denominator):
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
         result = numerator/denominator
     except ZeroDivisionError:
         print("Cannot divide by zero.")
