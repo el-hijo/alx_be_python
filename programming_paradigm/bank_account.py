55a6dfc293d60e2ef53f40997200b7d54d9de60a
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-10-05 12:34:11 +0100

    commiting both files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance=100):
+        self.account_balance = account_balance
+        
+    def deposit(self,amount):
+        self.account_balance += amount
+        return self.account_balance
+    def withdraw(self,amount):
+        if amount < self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+            
+    def display_balance():
+        print(self.account_balance)
+
+    
