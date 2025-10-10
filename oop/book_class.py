8507692fb27892e6c659db96fe19a6c9c8a0f8fc
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-10-10 10:15:47 +0100

    third commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,7 +2,7 @@ class Book:
     def __init__(self,title,author,year):
         self.title = title
         self.author = author
-        self.year = year
+        self.year = int(year)
     
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
