3127c7ab1c9f1e8bf8c756ef723e3fdb0854ee30
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-10-12 12:32:43 +0100

    second commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -6,6 +6,6 @@ class Calculator:
         return a + b
     
     @classmethod
-    def multiply(cls,a,b):
+    def multiply(cls, a, b):
         print(f"Calcutlation type: {cls.calculation_type}")
         return a * b
