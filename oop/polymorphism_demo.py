bcee874c7f9ace7bbd686e905fa8dbb1ac8fbc08
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-10-12 11:47:22 +0100

    initial commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must overide method")
+    
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius **2
+    
+rectangle = Rectangle(5,10)
+rectangle.area()
+circle = Circle(7)
+circle.area()
+        
+    
